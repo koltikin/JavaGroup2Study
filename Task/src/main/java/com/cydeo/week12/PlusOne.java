2401e053ae51c6c173b9ebb72d3fec15ee1eb45d
ziya <koltikin210@gmail.com>
2023-09-22 23:04:11 +0300

    one solution is added plusOne problem;

--- Task/src/main/java/com/cydeo/week12/PlusOne.java
+++ Task/src/main/java/com/cydeo/week12/PlusOne.java
@@ -6,11 +6,12 @@ public class PlusOne {
     public static void main(String[] args) {
         int[] num1 = {1,3,2};
         int[] num2 = {1,3,2,9};
-        int[] num3 = {1,9,9,9};
+        int[] num3 = {9,9,9,9};
 
         System.out.println(Arrays.toString(plus1(num1)));
-        System.out.println(Arrays.toString(plus1(num2)));
-        System.out.println(Arrays.toString(plus1(num3)));
+        System.out.println(Arrays.toString(plusOne(num2)));
+        System.out.println(Arrays.toString(plusOne(num3)));
+
 
     }
 
@@ -23,6 +24,21 @@ public class PlusOne {
         return String.valueOf(result).chars()
                 .map(Character::getNumericValue).toArray();
     }
+
+    private static int[] plusOne(int[] num) {
+        String strNum = "";
+        for (int i : num) {
+            strNum += i;
+        }
+        String result = String.valueOf(Integer.parseInt(strNum)+1);
+        char[] resultArr = result.toCharArray();
+        int[] newNum = new int[resultArr.length];
+        for (int i = 0; i < resultArr.length; i++) {
+            newNum[i] = Character.getNumericValue(resultArr[i]);
+        }
+            return newNum;
+        }
+
 
 
 }
