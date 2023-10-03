70c820c12105bb413ab31a843e2d63a33e9ecb31
ziya <koltikin210@gmail.com>
2023-10-03 10:30:08 +0300

    week 14 algo questions done

---
+++ Task/src/main/java/com/cydeo/week13/Parentheses.java
@@ -0,0 +1,18 @@
+package com.cydeo.week13;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parentheses {
+    public static void main(String[] args) {
+        String paren = "[]()";
+        System.out.println(isValidParentheses(paren));
+    }
+
+    private static boolean isValidParentheses(String paren) {
+
+
+        return false;
+        }
+}
